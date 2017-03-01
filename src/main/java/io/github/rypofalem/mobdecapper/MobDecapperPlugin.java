bb3ba82667b72217355f9ceb41998e480a0bb909
RypoFalem <rypofalem@gmail.com>
2017-03-01 13:44:19 -0600

    fixed spawning being too slow, added debug commands

--- src/main/java/io/github/rypofalem/mobdecapper/MobDecapperPlugin.java
+++ src/main/java/io/github/rypofalem/mobdecapper/MobDecapperPlugin.java
@@ -1,5 +1,6 @@
 package io.github.rypofalem.mobdecapper;
 
+import lombok.AllArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
@@ -8,6 +9,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,24 +17,23 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
 import lombok.EqualsAndHashCode;
 
 public class MobDecapperPlugin extends JavaPlugin implements Listener {
 
 	int localCap = 50;
-	int localRange = 128;
+	int localRange = 256;
 	HashMap<ChunkID, ChunkInfo> chunkMobs;
-	int counter = 0; //one second counter
+	int counterSeconds = 0; //one second counterSeconds
 
 
 	@Override
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(this, this);
 		chunkMobs = new HashMap<>();
-		Bukkit.getScheduler().runTaskTimer(this, () -> counter++, 1, 20);
+		Bukkit.getScheduler().runTaskTimer(this, () -> counterSeconds++, 1, 20);
 		saveDefaultConfig();
 		loadConfig();
 	}
@@ -48,9 +49,56 @@ public class MobDecapperPlugin extends JavaPlugin implements Listener {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(args != null && args.length > 0 && args[0].equalsIgnoreCase("reload")){
+		if(args == null || args.length < 1) return false;
+		if(args[0].equalsIgnoreCase("reload")){
 			loadConfig();
 			sender.sendMessage("Reloaded MobDecapper config.");
+			return true;
+		}else if(args[0].equalsIgnoreCase("players")){
+			String output = "";
+			for(World world : Bukkit.getWorlds()){
+				output += "\n" + world.getName() + "\n";
+				for(Player player : world.getPlayers()){
+					int count =  0;
+					for(Entity mob : player.getNearbyEntities(512, 256,512)){
+						if(isHostile(mob)) count++;
+					}
+					output+=String.format("    %s: %d\n", player.getName(), count);
+				}
+			}
+			sender.sendMessage(output);
+			return true;
+		} else if(args[0].equalsIgnoreCase("chunks")){
+			@AllArgsConstructor
+			class LocationCount implements Comparable<LocationCount>{
+				int count;
+				Location location;
+				@Override
+				public int compareTo(LocationCount other) {
+					return other.count - count;
+				}
+			}
+			Map<String, TreeSet<LocationCount>> worlds = new HashMap<>();
+			for(ChunkInfo chunk : chunkMobs.values()){
+				Location location = chunk.location;
+				String world = location.getWorld().getName();
+				if(!worlds.containsKey(world)){
+					worlds.put(world, new TreeSet<>());
+				}
+				int count = 0;
+				for(Entity mob : location.getWorld().getNearbyEntities(location, localRange, 256, localRange)){
+					if(isHostile(mob)) count++;
+				}
+				worlds.get(world).add(new LocationCount(count, location));
+			}
+			String output = "";
+			for(String world : worlds.keySet()){
+				output += "\n"+ world + ":\n";
+				for(LocationCount chunk : worlds.get(world)){
+					output += String.format("    %d, %d : %d\n", chunk.location.getBlockX(), chunk.location.getBlockZ(), chunk.count);
+				}
+			}
+			sender.sendMessage(output);
 			return true;
 		}
 		return false;
@@ -59,7 +107,9 @@ public class MobDecapperPlugin extends JavaPlugin implements Listener {
 	@EventHandler (priority = EventPriority.LOW, ignoreCancelled = true)
 	public void onMobSpawn(CreatureSpawnEvent event){
 		if(event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) return;
-		if(!isHostile(event.getEntity())) return;
+		if(!isHostile(event.getEntity())){
+			return;
+		}
 		Chunk chunk = event.getLocation().getChunk();
 		ChunkID id = new ChunkID(chunk);
 		if(!chunkMobs.containsKey(id)){
@@ -77,6 +127,7 @@ public class MobDecapperPlugin extends JavaPlugin implements Listener {
 
 	//100% OC plz don't steal
 	public static boolean isHostile(Entity entity) {
+		//if(!entity.isValid()) return false;
 		switch (entity.getType()) {
 			case CREEPER:
 			case SKELETON:
@@ -119,7 +170,7 @@ public class MobDecapperPlugin extends JavaPlugin implements Listener {
 		ChunkInfo(Location location){
 			this.location = location.getChunk().getBlock(7,0,7).getLocation();
 			tooManyMobs = true;
-			nextCheck = counter;
+			nextCheck = counterSeconds + cooldown;
 		}
 
 		ChunkInfo(Chunk chunk){
@@ -127,21 +178,23 @@ public class MobDecapperPlugin extends JavaPlugin implements Listener {
 		}
 
 		public boolean isTooManyMobs(){
-			if(counter <= nextCheck) return tooManyMobs;
+			if(counterSeconds <= nextCheck){
+				return tooManyMobs;
+			}
 			Collection<Entity> entityList = location.getWorld().getNearbyEntities(location, localRange, 256, localRange);
-			if(entityList.size() < localCap) return false;
+			if(entityList.size() < localCap){
+				return tooManyMobs = false;
+			}
 			int localMobs = 0;
 			for(Entity entity : entityList){
 				if(entity.isValid() && isHostile(entity)){
 					if(++localMobs >= localCap){
-						tooManyMobs = true;
-						nextCheck = counter + cooldown;
-						return true;
+						nextCheck = counterSeconds + cooldown; //wait 10 seconds before checking again if it's full
+						return tooManyMobs = true;
 					}
 				}
 			}
-			nextCheck = counter + cooldown;
-			return false;
+			return tooManyMobs = false;
 		}
 	}
 
